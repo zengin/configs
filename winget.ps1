64144507eeaf104af579b368a2491f4552924bcc
Mustafa Zengin <mzengin88@gmail.com>
2024-08-17 04:35:51 +0000

    Update winget.ps1

--- winget.ps1
+++ winget.ps1
@@ -4,31 +4,27 @@ winget install Git.Git
 winget install GitExtensionsTeam.GitExtensions
 winget install GitHub.GitHubDesktop
 winget install GitHub.cli
-winget install "Go Programming Language"
 winget install LINQPad.LINQPad.7
 winget install Microsoft.AzureCLI
-winget install Microsoft.AzureFunctionsCoreTools
 winget install Microsoft.AzureStorageExplorer
-winget install Microsoft.DotNet.SDK.7
-winget install Microsoft.DotNet.SDK.6
-winget install Microsoft.DotNet.SDK.5
-winget install Google.Chrome
-winget install Mozilla.Firefox
-winget install Notepad++.Notepad++
+winget install Microsoft.DotNet.SDK.8
 winget install OpenJS.NodeJS
-winget install Python.Python.3
-winget install Rustlang.Rust.GNU
 winget install Telerik.Fiddler.Classic
-winget install WinMerge.WinMerge
-winget install JetBrains.IntelliJIDEA.Community
-winget install Microsoft.OpenJDK.17
 winget install Microsoft.VisualStudio.2022.Enterprise
 winget install Microsoft.VisualStudioCode
 winget install Yubico.YubikeyManager
 winget install GnuPG.Gpg4win
 winget install JanDeDobbeleer.OhMyPosh
-winget install Postman.Postman
-winget install Microsoft.WindowsTerminal
+winget install Insomnia.Insomnia
+# winget install "Go Programming Language"
+# winget install Microsoft.AzureFunctionsCoreTools
+# winget install Python.Python.3
+# winget install Rustlang.Rust.GNU
+# winget install JetBrains.IntelliJIDEA.Community
+# winget install Microsoft.OpenJDK.17
+# winget install Google.Chrome
+# winget install Mozilla.Firefox
+# winget install Notepad++.Notepad++
 # winget install Graphviz
 # winget install "Git Credential Manager Core"
 # winget install "Microsoft Edge"
